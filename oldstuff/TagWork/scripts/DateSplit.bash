b38430ccd965f4c7e17944412eea8a38f62cbb09
Paul Houghton <paul4hough@gmail.com>
2016-03-02 07:17:39 -0700

    cleanup

--- oldstuff/TagWork/scripts/DateSplit.bash
+++
@@ -1,6 +0,0 @@
-for fn in * ; do
-  dt=`echo $fn | sed 's/^\(....-..-..\).*/\1/'`
-  echo $dt
-  [ -d "../Work/$dt" ] || mkdir "../Work/$dt"
-  mv "$fn" "../Work/$dt/" || exit
-done
--- oldstuff/TagWork/scripts/tsplit.pl
+++
@@ -1,8 +0,0 @@
-my $origComment = "Multi Line
-Comment for
-Testing
-Split function";
-
-foreach my $ln (split( /\n/,$origComment)) {
-  print "LN: $ln\n";
-}
